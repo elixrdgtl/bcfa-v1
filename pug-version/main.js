626e362b6eab68b765ca42a84b7a176a71c2b1bb
elijaveloper <elijaveloper@gmail.com>
2022-07-15 17:00:14 +0700

    converted to pug, served using ExpressJS

--- pug-version/main.js
+++ pug-version/main.js
@@ -1,15 +1,23 @@
-const app = require("express")();
-const port = 3030;
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+const port = process.env.PORT || 3030;
+
 const pug = require("pug");
 const compiled = pug.compileFile("index.pug");
 
-app.set("views","");
-app.set("view engine","pug");
+app.set("views", "");
+app.set("view engine", "pug");
 
-app.get("/",(req,res) => {
-  res.render("index");
+const publicDirectoryPath = path.join(__dirname, './public')
+app.use(express.static(publicDirectoryPath))
+
+app.get("/", (req, res) => {
+    res.render("index");
 })
 
-app.listen(port,() => {
-  console.log(`Server started listening at ${port}`)
+app.listen(port, () => {
+    console.log(`Server started listening at ${port}`)
 })
